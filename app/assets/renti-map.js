6e066b9b6dd40d7af561a2719f61e26438833eb4
kecoyo <kecoyo@163.com>
2016-09-04 09:04:28 +0800

    解决IOS8不能点击的问题

--- app/assets/renti-map.js
+++ app/assets/renti-map.js
@@ -6,16 +6,50 @@
 
         var panZoom;
 
-        var $popover = $('<div class="popover-box"><div class="popover" transition="modal"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div></div>');
+        var $popover = $('<div class="popover-box popover-hide"><div class="popover" transition="modal"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div></div>');
         $(element).after($popover);
-
-        var animate = '<animate attributeName="stroke-width" attributeType="XML"	values="2;10;2"	begin="0s" dur="2s"	repeatCount="indefinite"/>'
-            + '<animate attributeName="stroke-opacity" attributeType="CSS" values="1;0.6;1" begin="0s"	dur="2s" repeatCount="indefinite"/>';
 
         $(element).load(options.map, function () {
             var $svg = $("#svg");
             $svg.attr('style', 'display: inline; width: inherit; min-width: inherit; max-width: inherit; height: inherit; min-height: inherit; max-height: inherit;');
 
+
+            var $jingluo = $("#jingluo");
+            var $xuewei = $("#xuewei");
+
+            function clickXuewei() {
+                var xwid = $(this).attr('id');
+                showXwInfo(xwid);
+            }
+
+            $xuewei.children().on("click", clickXuewei);
+
+            function clickJingluo() {
+                var jlid = $(this).attr('id');
+                showJlInfo(jlid);
+                
+            }
+
+            $jingluo.children().addClass('jingluo').on("click", clickJingluo);
+            
+            // 是否显示经络
+            if (!options.jingluo) {
+                $jingluo.children().hide();
+            }
+            // 是否显示穴位
+            if (!options.xuewei) {
+                $xuewei.children().hide();
+            }
+            // 高亮经络
+            var disease = options.disease;
+            if (disease != null && highlights[disease] != null) {
+                $jingluo.children().hide();
+                var jls = highlights[disease];
+                for (var i in jls) {
+                    $jingluo.find('#' + jls[i]).show();
+                }
+            }
+            
             var eventsHandler = {
                 haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel'],
                 init: function (options) {
@@ -64,6 +98,15 @@
                     options.svgElement.addEventListener('touchmove', function (e) {
                         e.preventDefault();
                     });
+                    
+                    // Handle double tap
+                    this.hammer.on('tap', function (ev) {
+                        if($(ev.target).parent().attr('class') == 'jingluo'){
+                        	showJlInfo($(ev.target).parent().attr('id'))
+                        }else if($(ev.target).attr('class') == 'jingluo'){
+                        	showJlInfo($(ev.target).attr('id'))
+                        }
+                    });
                 },
                 destroy: function () {
                     this.hammer.destroy()
@@ -75,97 +118,57 @@
                 panEnabled: true,
                 controlIconsEnabled: false,
                 zoomScaleSensitivity: 0.5,
-                minZoom: 0.5,
-                maxZoom: 20,
+                minZoom: 0,
+                maxZoom: 50,
                 fit: 1,
                 center: 1,
                 customEventsHandler: eventsHandler
             });
-
-
-            var $jingluo = $("#jingluo");
-            var $xuewei = $("#xuewei");
-
-            function clickXuewei() {
-                var xwid = $(this).attr('id');
-                var data = xuewei[xwid];
-
-                var title = "<strong>穴位名称：</strong>" + data.name;
-                var content = "<p><strong>国际编码：</strong>" + data.code + "</p>";
-                content += "<p><strong>所属经络：</strong>" + (data.jl_name ? data.jl_name : "经外奇穴") + "</p>";
-                content += "<p><strong>穴位主治：</strong>" + (data.zhu_zhi ? data.zhu_zhi : "") + "</p>";
-
-                showPopover(title, content);
-            }
-
-            //$xuewei.children().on("tap", clickXuewei);
-            $xuewei.children().on("click", clickXuewei);
-
-            function clickJingluo() {
-
-                var jlid = $(this).attr('id');
-                var data = jingluo[jlid];
-
-                var title = "<strong>经络名称：</strong>" + data.name;
-                var content = "<p><strong>循行：</strong>" + data.xun_xing + "</p>";
-                content += "<p><strong>病候：</strong>" + data.bing_hou + "</p>";
-                content += "<p><strong>功能：</strong>" + data.gong_neng + "</p>";
-
-                showPopover(title, content);
-            }
-
-            //$jingluo.children().on("tap", clickJingluo);
-            $jingluo.children().on("click", clickJingluo);
-
-            // 是否显示经络
-            if (!options.jingluo) {
-                $jingluo.children().hide();
-            }
-            // 是否显示穴位
-            if (!options.xuewei) {
-                $xuewei.children().hide();
-            }
-            // 高亮经络
-            var disease = options.disease;
-            if (disease != null && highlights[disease] != null) {
-                $jingluo.children().hide();
-                var jls = highlights[disease];
-                for (var i in jls) {
-                    $jingluo.find('#' + jls[i]).show();
-                    $jingluo.find('#' + jls[i]).children().html(animate);
-                }
-            }
         });
+
+        function showJlInfo(jlid) {
+            var data = jingluo[jlid];
+
+            var title = "<strong>经络名称：</strong>" + data.name;
+            var content = "<p><strong>循行：</strong>" + data.xun_xing + "</p>";
+            content += "<p><strong>病候：</strong>" + data.bing_hou + "</p>";
+            content += "<p><strong>功能：</strong>" + data.gong_neng + "</p>";
+
+            showPopover(title, content);
+        }
+
+        function showXwInfo(xwid) {
+            var data = xuewei[xwid];
+
+            var title = "<strong>穴位名称：</strong>" + data.name;
+            var content = "<p><strong>国际编码：</strong>" + data.code + "</p>";
+            content += "<p><strong>所属经络：</strong>" + (data.jl_name ? data.jl_name : "经外奇穴") + "</p>";
+            content += "<p><strong>穴位主治：</strong>" + (data.zhu_zhi ? data.zhu_zhi : "") + "</p>";
+
+            showPopover(title, content);
+        }
 
         function showPopover(title, content) {
             $popover.find('.popover-title').html(title);
             $popover.find('.popover-content').html(content);
-            $popover.css('display', 'flex');
-            //$popover.find(".popover").animate({width: "80%", height: "70%"}, "fast", "swing", function () {});
+            $popover.removeClass('popover-hide').addClass('popover-show');
+            
         }
 
-        function hidePopover() {
-//          $popover.find(".popover").animate({width: "0px", height: "0px"}, "fast", "swing", function () {
-//              $popover.css('display', 'none');
-//          });
-			$popover.css('display', 'none');
+        function hidePopover() { 
+			$popover.removeClass('popover-show').addClass('popover-hide');
         }
 
 		function clickPopover() {
-			hidePopover();
-			return;
             var ele = event.srcElement || event.target;
-            var className = $(ele).attr('class');
-
-            if ($(ele).parents('#jingluo').length > 0)
-                return;
 
             if ($(ele).parents('.popover').length == 0) {
                 hidePopover();
             }
         }
-		//$popover.on("tap",clickPopover);
 		$popover.on("click",clickPopover);
+		
+//		showJlInfo("Kidney-2")
 
     };
 
